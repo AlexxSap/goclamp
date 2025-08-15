1b2557b505102117b044e4eccded750fa0b1b2b3
AlexxSap <alexxandrsap@mail.ru>
2025-08-15 13:27:17 +0300

    add comments

--- goclamp.go
+++ goclamp.go
@@ -4,10 +4,12 @@ import (
 	"cmp"
 )
 
+// Lesser is an interface that defines function Less. Less function reports whether the a less than b
 type Lesser interface {
 	Less(a, b any) bool
 }
 
+// clamp function that limits a value to a specified range [min, max]
 func clamp[T cmp.Ordered](min, max, value T) T {
 	if cmp.Less(value, min) {
 		return min
@@ -17,6 +19,7 @@ func clamp[T cmp.Ordered](min, max, value T) T {
 	return value
 }
 
+// clampFunc function that limits a value to a specified range [min, max] with specified less function (implementation of Lesser)
 func clampFunc[T any](min, max, value T, less func(a, b T) bool) T {
 	if less(value, min) {
 		return min
